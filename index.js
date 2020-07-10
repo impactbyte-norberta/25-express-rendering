4e3613efe768bbaeaf4cb4ece5fda0e2bf9c1714
Sarah Julia Gushef <sarahgushef@gmail.com>
2020-07-10 10:41:58 +0700

    Create example for routing in href

--- index.js
+++ index.js
@@ -16,6 +16,10 @@ app.get('/products', (req, res) => {
     res.sendFile(path.join(__dirname + '/views/products.html'));
 });
 
+app.get('/home', (req, res) => {
+    res.sendFile(path.join(__dirname + '/views/home.html'));
+});
+
 app.listen(PORT, () => {
     console.log(`Server runs on port ${PORT}`);
 });
